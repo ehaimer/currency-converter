d0a876b3a0414df9f0b6630c0440db7ee112ab99
ehaimer <y.ehaimer@gmail.com>
2023-09-20 03:13:53 -0400

    Add Fetch Api

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,20 @@
 import "./App.css"
 import CurrencyRow from "./components/CurrencyRow"
+import { useEffect } from "react"
 
 function App() {
   const apiKey = process.env.REACT_APP_API_KEY
   const baseURL = process.env.REACT_APP_BASE_URL
+  const endpointURL = `${baseURL}latest?access_key=${apiKey}`
+  console.log(endpointURL)
 
-  return (
+  useEffect(() => {
+    fetch(endpointURL)
+      .then((res) => res.json())
+      .then((data) => console.log(data))
+  }, [])
+
+  http: return (
     <main className="container">
       <h1>The Currency Converter</h1>
       <div className="from-row">
