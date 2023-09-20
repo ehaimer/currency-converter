dd79f7f5c897c5e3a4ba5d53ac549df6eb78b33f
ehaimer <y.ehaimer@gmail.com>
2023-09-20 01:22:30 -0400

    Prepare for implementation

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,9 @@
 import './App.css';
 
 function App() {
+  const apiKey = process.env.REACT_APP_API_KEY
+  const baseURL = process.env.REACT_APP_BASE_URL
+
   return (
     <div className="App">
     <h1>Hello World</h1>
