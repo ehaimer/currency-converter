cc081f217a18e259e8c2996d20d3361dfdd7e3d6
ehaimer <y.ehaimer@gmail.com>
2023-09-21 16:26:51 -0400

    Add New API

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,33 @@
 import "./App.css"
 import CurrencyRow from "./components/CurrencyRow"
-import { useEffect } from "react"
+import { useEffect, useState } from "react"
 
 function App() {
   const apiKey = process.env.REACT_APP_API_KEY
-  const baseURL = process.env.REACT_APP_BASE_URL
-  const endpointURL = `${baseURL}latest?access_key=${apiKey}`
+  const BASE_URL = "https://api.freecurrencyapi.com/v1/"
+  const endpointURL = `${BASE_URL}latest?apikey=${apiKey}`
   console.log(endpointURL)
+  //  &currencies=EUR%2CUSD%2CCAD&base_currency=BRL
+
+  const[currencies, setCurrencies] = useState({})
 
   useEffect(() => {
     fetch(endpointURL)
       .then((res) => res.json())
-      .then((data) => console.log(data))
+      .then((data) => setCurrencies(data.data))
   }, [])
 
-  http: return (
+  useEffect(() => {
+    const keys = Object.keys(currencies)
+    keys.map(key => console.log(key, currencies[key]))
+    const values = Object.values(currencies)
+    values.map(value => console.log(value))
+    for(let key in currencies) {
+      console.log(key, currencies[key])
+    }
+  }, [currencies])
+
+  return (
     <main className="container">
       <h1>The Currency Converter</h1>
       <div className="from-row">
