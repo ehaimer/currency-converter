4298f30d54380552f868d7fbff4d25d8afe4ad5f
ehaimer <y.ehaimer@gmail.com>
2023-09-22 01:33:40 -0400

    Fix fetch issue

--- src/App.jsx
+++ src/App.jsx
@@ -7,38 +7,23 @@ function App() {
   const apiKey = process.env.REACT_APP_API_KEY
   const BASE_URL = "https://api.freecurrencyapi.com/v1/"
   const endpointURL = `${BASE_URL}latest?apikey=${apiKey}`
-  // const [counterKeys, setCounterKeys] = useState([])
 
-  const [currencies, setCurrencies] = useState(() => {
-    JSON.parse(localStorage.getItem(TEMP_LOCAL_STORAGE_KEY)) ||
-      fetch(endpointURL)
-        .then((res) => res.json())
-        .then((data) => {
-          localStorage.setItem(
-            TEMP_LOCAL_STORAGE_KEY,
-            JSON.stringify(data.data)
-          )
-          return data.data
-        })
-  })
+  const [currencies, setCurrencies] = useState({})
 
   useEffect(() => {
-    fetch(endpointURL)
-      .then((res) => res.json())
-      .then((data) => {
-        setCurrencies(data.data)
-        localStorage.setItem(TEMP_LOCAL_STORAGE_KEY, JSON.stringify(data.data))
-      })
+    setCurrencies(
+      JSON.parse(localStorage.getItem(TEMP_LOCAL_STORAGE_KEY)) ||
+        fetch(endpointURL)
+          .then((res) => res.json())
+          .then((data) => {
+            localStorage.setItem(
+              TEMP_LOCAL_STORAGE_KEY,
+              JSON.stringify(data.data)
+            )
+            return data.data
+          })
+    )
   }, [])
-
-  // useEffect(() => {
-  //   if(currencies) {
-  //     const keys = Object.keys(currencies)
-  //     keys.map((key) => console.log(key, currencies[key]))
-  //     const values = Object.values(currencies)
-  //     values.map((value) => console.log(value))
-  //   }
-  // }, [currencies])
 
   //  &currencies=EUR%2CUSD%2CCAD&base_currency=BRL
 
