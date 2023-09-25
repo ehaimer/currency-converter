cefd4c5af7af9e4cceb1d485a724f5a43ae92910
ehaimer <y.ehaimer@gmail.com>
2023-09-25 13:56:01 -0400

    Round to 2 decimal point

--- src/CurrencyConverterApp.jsx
+++ src/CurrencyConverterApp.jsx
@@ -3,7 +3,7 @@ import { useEffect, useState, useRef } from "react"
 function CurrencyConverterApp() {
   const topCountryRef = useRef()
   const bottomCountryRef = useRef()
-  const apiKey = process.env.REACT_APP_API_KEY
+  const apiKey = "fca_live_xyKVzjJtnxqhivW9Fj644gOCFs4ZMBbUnagN0ubK"
   const BASE_URL = "https://api.freecurrencyapi.com/v1/"
   const endpointURL = `${BASE_URL}latest?apikey=${apiKey}`
   const [keys, setKeys] = useState([])
@@ -25,6 +25,7 @@ function CurrencyConverterApp() {
     const bottomValueAmount = parseFloat(bottomValue)
     const topCountry = topCountryRef.current.value
     const bottomCountry = bottomCountryRef.current.value
+
     if (
       (isNaN(topValueAmount) && isNaN(bottomValueAmount)) ||
       (!isNaN(topValueAmount) && !isNaN(bottomValueAmount))
@@ -35,25 +36,30 @@ function CurrencyConverterApp() {
       const URL = `${BASE_URL}latest?apikey=${apiKey}&currencies=${
         !isNaN(topValueAmount) ? bottomCountry : topCountry
       }&base_currency=${!isNaN(topValueAmount) ? topCountry : bottomCountry}`
+
       console.log(URL)
       const response = await fetch(URL)
       const data = await response.json()
       const filteredData = data.data
+
       console.log(filteredData)
+
       if (!isNaN(topValueAmount)) {
-        setBottomValue(filteredData[bottomCountry] * topValueAmount)
+        setBottomValue(
+          (filteredData[bottomCountry] * topValueAmount).toFixed(2)
+        )
       } else {
-        setTopValue(filteredData[topCountry] * bottomValueAmount)
+        setTopValue((filteredData[topCountry] * bottomValueAmount).toFixed(2))
       }
     }
   }
 
   return (
-    <main>
+    <main className="flex-grow">
       <form
         action="#"
         onSubmit={handleEnter}
-        className="w-[300px] my-16 mx-auto h-[350px] rounded-md shadow-all flex flex-col  items-center"
+        className="w-[300px] my-16 mx-auto h-[350px] rounded-md shadow-all flex flex-col items-center"
       >
         <h1 className="text-[1.3rem] whitespace-nowrap font-bold text-primary my-8">
           The Currency Converter
